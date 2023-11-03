0b02ac4efa050af771b463387affa67fec975a9c
Maciej Krawczyk <maciekrawczyk@gmail.com>
2023-11-02 22:40:39 -0500

    app set up

--- app.js
+++ app.js
@@ -9,19 +9,16 @@ const indexRouter = require('./routes/index')
 
 const app = express()
 
+
 // Set up views 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '\\views')
 
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/', indexRouter)
 
-
-
 app.listen(4444)
-
