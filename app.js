99309974c5a1d0c85f784609818313b0164462a4
Maciej Krawczyk <maciekrawczyk@gmail.com>
2023-12-21 16:41:54 -0600

    added secure acceptance web mobile

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
-const crypto = require('crypto')
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 // Set up routes
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const microform11Router = require('./routes/microform11');
+const secureAcceptanceWebMobileRouter = require('./routes/secureAcceptanceWebMobile');
 
+const app = express();
 
-const app = express()
+// Set up views
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set up views 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '\\views')
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/microform11', microform11Router);
+app.use('/secureAcceptanceWebMobile', secureAcceptanceWebMobileRouter);
 
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.locals.generateUUIDv4 = () => {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+    );
+};
 
-app.use('/', indexRouter)
+app.locals.getTimeStamp = () => {
+    const d = new Date();
+    const yy = ('' + d.getFullYear()).slice(-2);
+    const mm = ('0' + d.getMonth()).slice(-2);
+    const dd = ('0' + d.getDate()).slice(-2);
+    const h = ('0' + d.getHours()).slice(-2);
+    const m = ('0' + d.getMinutes()).slice(-2);
+    const s = ('0' + d.getSeconds()).slice(-2);
+    return yy + mm + dd + '_' + h + m + s;
+};
 
-app.listen(4444)
+app.listen(4444);
---
+++ data/configuration.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+
+/*
+* Merchant configuration properties are taken from Configuration module
+*/
+
+// common parameters
+const AuthenticationType = 'http_signature';
+const RunEnvironment = 'apitest.cybersource.com';
+const MerchantId = process.env.CYBS_MID;
+
+// http_signature parameters
+const MerchantKeyId = process.env.CYBS_KEY_ID;
+const MerchantSecretKey = process.env.CYBS_SHARED_SECRET;
+
+// jwt parameters
+const KeysDirectory = 'Resource';
+const KeyFileName = 'testrest';
+const KeyAlias = 'testrest';
+const KeyPass = 'testrest';
+
+//meta key parameters
+const UseMetaKey = false;
+const PortfolioID = '';
+
+// logging parameters
+const EnableLog = false;
+const LogFileName = 'cybs';
+const LogDirectory = 'log';
+const LogfileMaxSize = '5242880'; //10 MB In Bytes
+const EnableMasking = true;
+
+/*
+PEM Key file path for decoding JWE Response Enter the folder path where the .pem file is located.
+It is optional property, require adding only during JWE decryption.
+*/
+const PemFileDirectory = 'Resource/NetworkTokenCert.pem';
+
+// Constructor for Configuration
+function Configuration() {
+
+    var configObj = {
+        'authenticationType': AuthenticationType,
+        'runEnvironment': RunEnvironment,
+
+        'merchantID': MerchantId,
+        'merchantKeyId': MerchantKeyId,
+        'merchantsecretKey': MerchantSecretKey,
+
+        'keyAlias': KeyAlias,
+        'keyPass': KeyPass,
+        'keyFileName': KeyFileName,
+        'keysDirectory': KeysDirectory,
+
+        'useMetaKey': UseMetaKey,
+        'portfolioID': PortfolioID,
+        // 'pemFileDirectory': PemFileDirectory,
+
+        'logConfiguration': {
+            'enableLog': EnableLog,
+            'logFileName': LogFileName,
+            'logDirectory': LogDirectory,
+            'logFileMaxSize': LogfileMaxSize,
+            'loggingLevel': 'debug',
+            'enableMasking': EnableMasking
+        }
+    };
+    return configObj;
+
+}
+
+
+module.exports = Configuration;
